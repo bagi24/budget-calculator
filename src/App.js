5e530b6ce6bff54e1cb6f9b47183f5ad2faf73ee
bagi24 <bagrat.injgia24@gmail.com>
2023-06-19 12:17:22 +0400

    functions clearItems and handleDelete done

--- src/App.js
+++ src/App.js
@@ -56,6 +56,32 @@ function App() {
       });
     }
   };
+
+  //clear all items
+
+  const clearItems = () => {
+    console.log("cleared all items");
+
+    setExpenses([]);
+    handleAlert({ type: "danger", text: "All item deleted" });
+  };
+
+  //handle Delete
+
+  const handleDelete = (id) => {
+    console.log("item deleted : ${id}");
+
+    let tempExpenses = expenses.filter((item) => item.id !== id);
+
+    setExpenses(tempExpenses);
+    handleAlert({ type: "danger", text: "item deleted" });
+  };
+
+  //handle Edit
+  const handleEdit = (id) => {
+    console.log("item Edited : ${id}");
+  };
+
   return (
     <div>
       {alert.show && <Alert type={alert.type} text={alert.text} />}
@@ -69,7 +95,12 @@ function App() {
           handleCharge={handleCharge}
           handleSubmit={handleSubmit}
         />
-        <ExpenseList expenses={expenses} />
+        <ExpenseList
+          expenses={expenses}
+          handleDelete={handleDelete}
+          handleEdit={handleEdit}
+          clearItems={clearItems}
+        />
       </main>
 
       <h1>
